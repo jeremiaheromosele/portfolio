40bfa073eab4db3758be3b45d3f7d9af181c425d
alonsoeromosele@gmail.com <alonsoeromosele@gmail.com>
2023-08-10 17:59:11 +0100

    new updates

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,10 +1,11 @@
+import Navbar from "./Components/Navbar";
+
+
 
 function App() {
   return (
     <div>
-          <h1 className="text-3xl font-bold underline text-red-600">
-      Hello world!
-    </h1>
+       <Navbar/>
     </div>
   );
 }
