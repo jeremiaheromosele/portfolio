2628a6c7d2b028795cced452e3c433a64362f9a5
alonsoeromosele@gmail.com <alonsoeromosele@gmail.com>
2023-08-18 16:36:08 +0100

    portfolio update

--- my-portfolio/src/Components/Portfolio.js
+++ my-portfolio/src/Components/Portfolio.js
@@ -6,20 +6,27 @@ import basicland from '../Assets/basicland.jpg'
 
 
 const Portfolio = () => {
+    const openInNewTab = url =>{
+        window.open(url, '_blank','noopener,noreferrer');
+    };
     const project =[
         {
             id:1,
-            src:foodapp
+            src:foodapp,
+            code: 'https://github.com/jeremiaheromosele/Meta',
         },
         {
             id:1,
-            src:portfolio
+            src:portfolio,
+            code: 'https://github.com/jeremiaheromosele/portfolio',
         },{
             id:1,
-            src:reactapp
+            src:reactapp,
+            code: 'https://github.com/jeremiaheromosele/Meta-capstone-project',
         },{
             id:1,
-            src:basicland
+            src:basicland,
+            code: 'https://github.com/jeremiaheromosele/React-project',
         },
     ]
   return (
@@ -30,12 +37,11 @@ const Portfolio = () => {
             <p className='py-6'>Check out some of my projects</p>
         </div>
         <div className='grid sm:grid-cols-2 md:grid-cols-4 gap-4 px-4 sm:px-0 '>
-            {project.map(({ id, src }) => (
+            {project.map(({ id, src,code }) => (
                 <div key={id} className='w-60 mx-auto shadow-md shadow-gray-600 rounded-lg hover:shadow-yellow-500'>
                     <img src={src} alt='project 1' className='rounded-md duration-200 h-40 w-full object-fill hover:scale-105' />
                     <div className='flex items-center justify-center'>
-                        <button className='w-1/2 px-0 py-2 m-2 duration-200 hover:scale-105'>Demo</button>
-                        <button className='w-1/2 px-0 py-2 m-2 duration-200 hover:scale-105'>Code</button>
+                        <button onClick={()=> openInNewTab(code)} className='w-1/2 px-0 py-2 m-2 duration-200 hover:scale-105'>Code</button>
                     </div>
                 </div>
             ))}
